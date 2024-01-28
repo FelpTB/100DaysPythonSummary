1b5bbe9a6825045ad16e08f927cd3d3d359bb408
Felipe <114826296+FelpTB@users.noreply.github.com>
2024-01-28 15:31:50 -0300

    Add files via upload

---
+++ Day6(While, Bocos, funções).py
@@ -0,0 +1,15 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+def my_function():
+    print("Hello")
+    print("byee")
+
+my_function() 
+
+x = 0
+
+while x < 10:
+    print(x)
+    x += 1
